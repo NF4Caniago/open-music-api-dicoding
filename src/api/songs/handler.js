76db63a199bd36fd6e38d81da9c1ed5267a04c3f
Afif <afifilhamcaniago@gmail.com>
2023-02-20 15:58:49 +0700

    add get songs

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -6,7 +6,7 @@ class SongsHandler {
     this._service = service;
     this._validator = validator;
     this.postSongHandler = this.postSongHandler.bind(this);
-    // this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this);
     // this.getSongsByIdHandler = this.getSongsByIdHandler.bind(this);
     // this.putSongsByIdHandler = this.putSongsByIdHandler.bind(this);
     // this.deleteSongsByIdHandler = this.deleteSongsByIdHandler.bind(this);
@@ -46,7 +46,33 @@ class SongsHandler {
     }
   }
 
-  // async getSongsHandler(request, h) {}
+  async getSongsHandler(request, h) {
+    try {
+      const songs = await this._service.getSongs();
+      const response = h.response({
+        status: 'success',
+        data: {
+          songs,
+        },
+      }).code(200);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+        return response;
+      }
+      // server error
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      }).code(500);
+      console.log(error.message);
+      return response;
+    }
+  }
 
   // async getSongsByIdHandler(request, h) {}
 
--- src/service/postgres/SongsService.js
+++ src/service/postgres/SongsService.js
@@ -27,6 +27,11 @@ class SongsService {
     }
     return result.rows[0].id;
   }
+
+  async getSongs() {
+    const result = await this._pool.query('SELECT id, title, performer FROM songs');
+    return result.rows;
+  }
 }
 
 module.exports = SongsService;
